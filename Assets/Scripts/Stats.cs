65a1629003343f86a3846650a3ca58700f780ee0
EgorCita <murashkoegor@yandex.ru>
2021-12-05 20:41:18 +0300

    Add caterpillar
    Добавил первую гусеничку <3

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int maxhealth;
+    public int health;
+    public float speed;
+    public int dmg;
+    public bool isInvinsible = false;
+
+    private void Start()
+    {
+        health = maxhealth;
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
