551bae0539cece37efe32ab24000b7edb017dfbc
latinrice <92548405+latinrice@users.noreply.github.com>
2022-05-10 19:12:06 +0300

    Animations Stage2 Completed

--- Assets/Scripts/Dragon/Stage2/dragon_fireRightBehavior.cs
+++ Assets/Scripts/Dragon/Stage2/dragon_fireRightBehavior.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class dragon_fireRightBehavior : StateMachineBehaviour
 {
+    private GameObject headRight;
+    private GameObject player;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         animator.GetComponent<Dragon>().fireAttackCount++;
-        GameObject.Find("headRight").GetComponent<Stats>().isInvulnerable = false;
+        headRight = GameObject.Find("headRight");
+        headRight.transform.position += new Vector3(0.8f, 0, 0);
+        headRight.GetComponent<Stats>().isInvulnerable = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (player.transform.position.x > headRight.transform.position.x)
+            headRight.GetComponent<Stats>().isInvulnerable = true;
+        else
+            headRight.GetComponent<Stats>().isInvulnerable = false;
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        GameObject.Find("headRight").GetComponent<Stats>().isInvulnerable = true;
+        headRight.GetComponent<Stats>().isInvulnerable = true;
+        headRight.transform.position += new Vector3(-0.8f, 0, 0);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
